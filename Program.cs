30500cfcfa0d520e4a361bfc66c55cb7dc3e012f
macg30 <macg30@users.noreply.github.com>
2021-05-14 00:54:59 +0200

    Add json to build and get config var

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+// using Microsoft.Extensions.Configuration.FileExtensions;
+using Microsoft.Extensions.Configuration.Json;
+
+using Microsoft.Azure;
+using Microsoft.Azure.Storage;
+using Microsoft.Azure.Storage.Blob;
+
+namespace BlobConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Config Builder
+            var build = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json");
+            
+            //Set config
+            IConfiguration config = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json", true, true)
+            .Build();
+
+            //Get connection string
+            string getConnString = config["connectionstring"];
+            Console.WriteLine(getConnString);
+
+            // CloudStorageAccount StorageAccount = CloudStorageAccount.Parse();
+        }
+    }
+}
